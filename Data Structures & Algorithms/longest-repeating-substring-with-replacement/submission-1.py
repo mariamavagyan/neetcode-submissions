c2dfd2e642f592c1b3e2440abe8b0589ebadc909
mariam <36089981+mariamavagyan@users.noreply.github.com>
2026-09-15 17:39:43 -0400

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # hashmap to count occurences of each character
+        count = {}
+        res = 0
+        left = 0
+
+        for right in range(len(s)):
+            # for the first time, when we see a character, it is not in the hashmap yet. When we do .get, it will give us an error. This way, it returns the default value of zero.
+            count[s[right]] = 1 + count.get(s[right], 0)
+
+            # check if the current window is valid
+            # window_len = right - left + 1
+            # while the window is not valid, update the left pointer
+            while (right - left + 1) - max(count.values()) > k:
+                count[s[left]] -= 1 # decremenet the count of the character on the left
+                left += 1
+
+            res = max(res, right - left + 1)
+        return res
